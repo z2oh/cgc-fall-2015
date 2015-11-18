cf7552bd47e16ad423013cfb19e950417c1b2317
Jeremy Day <jaday@email.sc.edu>
2015-11-18 18:55:38 -0500

    Added soft pressure plates and the finish point and designed the first level. Still a lot of work to do, but this can be considered version 0.1a.

---
+++ Assets/Scripts/Gameplay/Objects/Finish.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Finish : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.gameObject.GetComponent<ObjectInfo>().weight > 5)
+        {
+            print("quit");
+            Application.Quit();
+        }
+    }
+}
---
+++ Assets/Scripts/Gameplay/Objects/SoftPressurePlate.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoftPressurePlate : MonoBehaviour
+{
+    public GameObject obj;
+
+    int numActive = 0;
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.gameObject.GetComponent<ObjectInfo>() != null)// && coll.gameObject.GetComponent<ObjectInfo>().weight > 5)
+        {
+            numActive++;
+            if (numActive == 1)
+            {
+                transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y * 0.5f, transform.localScale.z);
+                obj.GetComponent<Activator>().Activate();
+            }
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D coll)
+    {
+        if (coll.gameObject.GetComponent<ObjectInfo>() != null)// && coll.gameObject.GetComponent<ObjectInfo>().weight > 5)
+        {
+            numActive--;
+            if (numActive == 0)
+            {
+                transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y * 2f, transform.localScale.z);
+                obj.GetComponent<Activator>().Deactivate();
+            }
+        }
+    }
+}
