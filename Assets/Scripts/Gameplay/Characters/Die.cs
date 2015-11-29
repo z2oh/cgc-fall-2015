8079bf10c08693604177a4f01af4f90fd05a6c9c
Jeremy Day <jaday@email.sc.edu>
2015-11-29 14:23:14 -0500

    Added 'heart' collision detection. If a door collides with the center of a player or crab the level will restart.

---
+++ Assets/Scripts/Gameplay/Characters/Die.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Die : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.gameObject.layer == 9 || coll.gameObject.layer == 10)
+        {
+            GameObject cam = GameObject.Find("Main Camera");
+            cam.GetComponent<ActiveController>().shouldLoad = true;
+            cam.GetComponent<LoadLevel>().Load();
+        }
+    }
+}
